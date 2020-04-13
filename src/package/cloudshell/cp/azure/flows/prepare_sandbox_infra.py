1e64672335f2ea46d5d2fa6e8c8590cebc119164
Anthony Poddubny <anton.p@qualisystems.com>
2020-04-14 00:03:35 +0300

    add cancellation context manager module

--- src/package/cloudshell/cp/azure/flows/prepare_sandbox_infra.py
+++ src/package/cloudshell/cp/azure/flows/prepare_sandbox_infra.py
@@ -7,22 +7,22 @@ from package.cloudshell.cp.azure.actions.ssh_key_pair import SSHKeyPairActions
 from package.cloudshell.cp.azure.actions.network_security_group import NetworkSecurityGroupActions
 from package.cloudshell.cp.azure.actions.network import NetworkActions
 
+
 # TODO: run different commands in threads in the AbstractPrepareSandboxInfraFlow !!!!!!!!!!!!!!!!
-
-
 class AzurePrepareSandboxInfraFlow(AbstractPrepareSandboxInfraFlow):
-    def __init__(self, resource_config, azure_client, reservation_info, logger):
+    def __init__(self, resource_config, azure_client, reservation_info, cancellation_manager, logger):
         """
 
         :param resource_config:
         :param azure_client:
         :param reservation_info:
+        :param cancellation_manager:
         :param logger:
         """
-        self._resource_config = resource_config
+        AbstractPrepareSandboxInfraFlow.__init__(self, resource_config=resource_config, logger=logger)
         self._azure_client = azure_client
         self._reservation_info = reservation_info
-        self._logger = logger
+        self._cancellation_manager = cancellation_manager
 
     def prepare_cloud_infra(self, action):
         """
@@ -57,9 +57,6 @@ class AzurePrepareSandboxInfraFlow(AbstractPrepareSandboxInfraFlow):
         :param list[cloudshell.cp.core.models.PrepareSubnet] actions:
         :return:
         """
-        # todo: check that it will create the same subnets as the old Azure shell !!!!!
-        # todo: ??? make partial() for all calls, run them in loop and check cancellation context
-
         resource_group_name = self._reservation_info.get_resource_group_name()
         nsg_name = self._reservation_info.get_network_security_group_name()
         tags = self._reservation_info.get_tags()
@@ -76,12 +73,11 @@ class AzurePrepareSandboxInfraFlow(AbstractPrepareSandboxInfraFlow):
                                                              region=self._resource_config.region,
                                                              tags=tags)
 
-        nsg = nsg_actions.create_sandbox_network_security_group(nsg_name=nsg_name,
-                                                                resource_group_name=resource_group_name,
-                                                                region=self._resource_config.region,
-                                                                tags=tags)
-
-        # todo: fix cancellation context checking - do it in the end of the command !!!
+        with self._cancellation_manager:
+            nsg = nsg_actions.create_sandbox_network_security_group(nsg_name=nsg_name,
+                                                                    resource_group_name=resource_group_name,
+                                                                    region=self._resource_config.region,
+                                                                    tags=tags)
 
         def action_with_cidr(action):
             # todo: check if we have PrepareCloudInfra there and replace this code !!!!!!!
@@ -100,41 +96,46 @@ class AzurePrepareSandboxInfraFlow(AbstractPrepareSandboxInfraFlow):
         # Enable access from sandbox traffic for all subnets. Note that specific VMs can block sandbox traffic using
         # the VM network security group, which is created per VM.
         for action in actions:
-            nsg_actions.create_nsg_allow_rule(
-                rule_name=f"Allow_Sandbox_Traffic_To_{action.actionParams.cidr.replace('/', '-')}",
-                resource_group_name=resource_group_name,
-                nsg_name=nsg_name,
-                src_address=sandbox_cidr,
-                dst_address=action.actionParams.cidr,
-                start_from=2000)
+            with self._cancellation_manager:
+                nsg_actions.create_nsg_allow_rule(
+                    rule_name=f"Allow_Sandbox_Traffic_To_{action.actionParams.cidr.replace('/', '-')}",
+                    resource_group_name=resource_group_name,
+                    nsg_name=nsg_name,
+                    src_address=sandbox_cidr,
+                    dst_address=action.actionParams.cidr,
+                    start_from=2000)
 
         # PRIORITY 2xxx:
         # Block access from internet to private subnets
         for action in self._get_private_subnet_actions(subnet_actions=actions):
-            nsg_actions.create_nsg_deny_rule(
-                rule_name=f"Deny_Internet_Traffic_To_Private_Subnet_{action.actionParams.cidr.replace('/', '-')}",
-                resource_group_name=resource_group_name,
-                nsg_name=nsg_name,
-                dst_address=action.actionParams.cidr,
-                start_from=2000)
+            with self._cancellation_manager:
+                nsg_actions.create_nsg_deny_rule(
+                    rule_name=f"Deny_Internet_Traffic_To_Private_Subnet_{action.actionParams.cidr.replace('/', '-')}",
+                    resource_group_name=resource_group_name,
+                    nsg_name=nsg_name,
+                    dst_address=action.actionParams.cidr,
+                    start_from=2000)
 
         # PRIORITY 4xxx:
         # Allow inbound traffic from additional management networks (can configure on Azure cloud provider resource
         # that additional networks are allowed to communicate with subnets and vms)
         for mgmt_network in self._resource_config.additional_mgmt_networks:
-            nsg_actions.create_nsg_allow_rule(
-                rule_name=f"Allow_{mgmt_network.replace('/', '-')}_To_{sandbox_cidr.replace('/', '-')}",
-                resource_group_name=resource_group_name,
-                nsg_name=nsg_name,
-                src_address=mgmt_network,
-                dst_address=sandbox_cidr,
-                start_from=4000)
+            with self._cancellation_manager:
+                nsg_actions.create_nsg_allow_rule(
+                    rule_name=f"Allow_{mgmt_network.replace('/', '-')}_To_{sandbox_cidr.replace('/', '-')}",
+                    resource_group_name=resource_group_name,
+                    nsg_name=nsg_name,
+                    src_address=mgmt_network,
+                    dst_address=sandbox_cidr,
+                    start_from=4000)
 
-        mgmt_vnet = network_actions.get_mgmt_virtual_network(
-            resource_group_name=self._resource_config.management_group_name)
+        with self._cancellation_manager:
+            mgmt_vnet = network_actions.get_mgmt_virtual_network(
+                resource_group_name=self._resource_config.management_group_name)
 
-        sandbox_vnet = network_actions.get_sandbox_virtual_network(
-            resource_group_name=self._resource_config.management_group_name)
+        with self._cancellation_manager:
+            sandbox_vnet = network_actions.get_sandbox_virtual_network(
+                resource_group_name=self._resource_config.management_group_name)
 
         mgmt_vnet_cidr = mgmt_vnet.address_space.address_prefixes[0]
         sandbox_vnet_cidr = sandbox_vnet.address_space.address_prefixes[0]
@@ -142,35 +143,38 @@ class AzurePrepareSandboxInfraFlow(AbstractPrepareSandboxInfraFlow):
         # PRIORITY 4080:
         # Allow MGMT vNET CIDR inbound traffic. Basically providing access to the infrastructure to manage
         # elements in the sandbox
-        nsg_actions.create_nsg_allow_rule(
-            rule_name=f"Allow_{mgmt_vnet_cidr.replace('/', '-')}_To_{sandbox_cidr.replace('/', '-')}",
-            resource_group_name=resource_group_name,
-            nsg_name=nsg_name,
-            src_address=mgmt_vnet_cidr,
-            dst_address=sandbox_cidr,
-            start_from=4080)
+        with self._cancellation_manager:
+            nsg_actions.create_nsg_allow_rule(
+                rule_name=f"Allow_{mgmt_vnet_cidr.replace('/', '-')}_To_{sandbox_cidr.replace('/', '-')}",
+                resource_group_name=resource_group_name,
+                nsg_name=nsg_name,
+                src_address=mgmt_vnet_cidr,
+                dst_address=sandbox_cidr,
+                start_from=4080)
 
         # PRIORITY 4090
         # Deny inbound traffic from Sandbox vNET (the azure account vNET with  which subnets from all sandboxes
         # are associated). The idea is to block traffic from other sandboxes in the account.
-        nsg_actions.create_nsg_deny_rule(
-            rule_name="Deny_Traffic_From_Other_Sandboxes_To_Sandbox_CIDR",
-            resource_group_name=resource_group_name,
-            nsg_name=nsg_name,
-            src_address=sandbox_vnet_cidr,
-            dst_address=sandbox_cidr,
-            start_from=4090)
+        with self._cancellation_manager:
+            nsg_actions.create_nsg_deny_rule(
+                rule_name="Deny_Traffic_From_Other_Sandboxes_To_Sandbox_CIDR",
+                resource_group_name=resource_group_name,
+                nsg_name=nsg_name,
+                src_address=sandbox_vnet_cidr,
+                dst_address=sandbox_cidr,
+                start_from=4090)
 
         # Create additional subnets requested by server
         for subnet_action in actions:
-            subnet_name = self._prepare_subnet_name(resource_group_name=resource_group_name,
-                                                    subnet_cidr=subnet_action.actionParams.cidr)
+            with self._cancellation_manager:
+                subnet_name = self._prepare_subnet_name(resource_group_name=resource_group_name,
+                                                        subnet_cidr=subnet_action.actionParams.cidr)
 
-            network_actions.create_subnet(subnet_name=subnet_name,
-                                          cidr=subnet_action.actionParams.cidr,
-                                          vnet=sandbox_vnet,
-                                          resource_group_name=self._resource_config.management_group_name,
-                                          network_security_group=nsg)
+                network_actions.create_subnet(subnet_name=subnet_name,
+                                              cidr=subnet_action.actionParams.cidr,
+                                              vnet=sandbox_vnet,
+                                              resource_group_name=self._resource_config.management_group_name,
+                                              network_security_group=nsg)
 
     def _prepare_subnet_name(self, resource_group_name, subnet_cidr):
         """
@@ -198,20 +202,24 @@ class AzurePrepareSandboxInfraFlow(AbstractPrepareSandboxInfraFlow):
         storage_account_actions = StorageAccountActions(azure_client=self._azure_client,
                                                         logger=self._logger)
 
-        storage_account_actions.create_sandbox_storage_account(storage_account_name=storage_account_name,
-                                                               resource_group_name=resource_group_name,
-                                                               region=self._resource_config.region,
-                                                               tags=tags)
+        with self._cancellation_manager:
+            storage_account_actions.create_sandbox_storage_account(storage_account_name=storage_account_name,
+                                                                   resource_group_name=resource_group_name,
+                                                                   region=self._resource_config.region,
+                                                                   tags=tags)
 
-        private_key, public_key = ssh_actions.create_ssh_key_pair()
+        with self._cancellation_manager:
+            private_key, public_key = ssh_actions.create_ssh_key_pair()
 
-        ssh_actions.save_ssh_public_key(resource_group_name=resource_group_name,
-                                        storage_account_name=storage_account_name,
-                                        public_key=public_key)
+        with self._cancellation_manager:
+            ssh_actions.save_ssh_public_key(resource_group_name=resource_group_name,
+                                            storage_account_name=storage_account_name,
+                                            public_key=public_key)
 
-        ssh_actions.save_ssh_private_key(resource_group_name=resource_group_name,
-                                         storage_account_name=storage_account_name,
-                                         private_key=private_key)
+        with self._cancellation_manager:
+            ssh_actions.save_ssh_private_key(resource_group_name=resource_group_name,
+                                             storage_account_name=storage_account_name,
+                                             private_key=private_key)
 
         return private_key
 
