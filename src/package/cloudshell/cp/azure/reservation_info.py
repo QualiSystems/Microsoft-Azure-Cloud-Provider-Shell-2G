cc8877be714d25d6c8330ba4ba6080e7e08581b8
Anthony Poddubny <anton.p@qualisystems.com>
2020-04-18 19:37:19 +0300

    add DeployApp models for the Deploy VM command

--- src/package/cloudshell/cp/azure/reservation_info.py
+++ src/package/cloudshell/cp/azure/reservation_info.py
@@ -1,22 +1,6 @@
-from dataclasses import dataclass
+from cloudshell.cp.core.reservation_info import ReservationInfo
 
 from package.cloudshell.cp.azure.utils.name_generator import generate_name
-
-
-# todo: move it to the cloudshell-cp-core
-@dataclass
-class ReservationInfo:
-    reservation_id: str
-    owner: str
-    blueprint: str
-    domain: str
-
-    @classmethod
-    def from_context(cls, context):
-        return cls(reservation_id=context.reservation_id,
-                   owner=context.owner_user,
-                   blueprint=context.environment_name,
-                   domain=context.domain)
 
 
 class AzureReservationInfo(ReservationInfo):
