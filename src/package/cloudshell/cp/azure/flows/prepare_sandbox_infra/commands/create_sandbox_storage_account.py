0769d69d9385ebf0913e57aa5f5b75e77e62d899
Anthony Poddubny <anton.p@qualisystems.com>
2020-04-21 22:22:34 +0300

    refactor Prepare Sandbox Infra flow

---
+++ src/package/cloudshell/cp/azure/flows/prepare_sandbox_infra/commands/create_sandbox_storage_account.py
@@ -0,0 +1,31 @@
+from package.cloudshell.cp.azure.utils.rollback import RollbackCommand
+
+
+class CreateSandboxStorageAccountCommand(RollbackCommand):
+    def __init__(self, rollback_manager, cancellation_manager, storage_actions, storage_account_name,
+                 resource_group_name, region, tags):
+        """
+
+        :param rollback_manager:
+        :param cancellation_manager:
+        :param storage_actions:
+        :param storage_account_name:
+        :param resource_group_name:
+        :param region:
+        :param tags:
+        """
+        super().__init__(rollback_manager=rollback_manager, cancellation_manager=cancellation_manager)
+        self._storage_actions = storage_actions
+        self._storage_account_name = storage_account_name
+        self._resource_group_name = resource_group_name
+        self._region = region
+        self._tags = tags
+
+    def _execute(self):
+        self._storage_actions.create_sandbox_storage_account(storage_account_name=self._storage_account_name,
+                                                             resource_group_name=self._resource_group_name,
+                                                             region=self._region,
+                                                             tags=self._tags)
+
+    def rollback(self):
+        pass
---
+++ src/package/cloudshell/cp/azure/flows/prepare_sandbox_infra/commands/create_subnet.py
@@ -0,0 +1,37 @@
+from package.cloudshell.cp.azure.utils.rollback import RollbackCommand
+
+
+class CreateSubnetCommand(RollbackCommand):
+    def __init__(self, rollback_manager, cancellation_manager, network_actions, vnet, cidr,
+                 resource_group_name, management_group_name, network_security_group):
+        """
+
+        :param rollback_manager:
+        :param cancellation_manager:
+        :param network_actions:
+        :param vnet:
+        :param cidr:
+        :param resource_group_name:
+        :param management_group_name:
+        :param network_security_group:
+        """
+        super().__init__(rollback_manager=rollback_manager, cancellation_manager=cancellation_manager)
+        self._network_actions = network_actions
+        self._vnet = vnet
+        self._cidr = cidr
+        self._resource_group_name = resource_group_name
+        self._management_group_name = management_group_name
+        self._network_security_group = network_security_group
+
+    def _prepare_subnet_name(self):
+        return f"{self._resource_group_name}_{self._cidr}".replace(' ', '').replace('/', '-')
+
+    def _execute(self):
+        self._network_actions.create_subnet(subnet_name=self._prepare_subnet_name(),
+                                            cidr=self._cidr,
+                                            vnet=self._vnet,
+                                            resource_group_name=self._management_group_name,
+                                            network_security_group=self._network_security_group)
+
+    def rollback(self):
+        pass
