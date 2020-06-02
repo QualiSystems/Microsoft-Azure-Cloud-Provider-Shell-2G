9d4ce395b74e3fe0f65cc73996f4cd47d659dce1
Anthony Poddubny <anton.p@qualisystems.com>
2020-06-03 01:20:16 +0300

    rework azure tags preparation

--- src/package/cloudshell/cp/azure/reservation_info.py
+++ src/package/cloudshell/cp/azure/reservation_info.py
@@ -2,18 +2,7 @@ from cloudshell.cp.core.reservation_info import ReservationInfo
 
 
 class AzureReservationInfo(ReservationInfo):
-    # todo: remove reservation id from the sandbox name
     SANDBOX_NSG_NAME_TPL = "NSG_sandbox_all_subnets_{reservation_id}"
-
-    class TagNames:
-        created_by = "CreatedBy"
-        owner = "Owner"
-        blueprint = "Blueprint"
-        sandbox_id = "SandboxId"
-        domain = "Domain"
-
-    class TagValues:
-        created_by = "CloudShell"
 
     def get_resource_group_name(self):
         """
@@ -35,17 +24,3 @@ class AzureReservationInfo(ReservationInfo):
         :rtype: str
         """
         return self.SANDBOX_NSG_NAME_TPL.format(reservation_id=self.reservation_id)
-
-    def get_tags(self):
-        """
-
-        :return:
-        :rtype: dict[str, str]
-        """
-        return {
-            self.TagNames.created_by: self.TagValues.created_by,
-            self.TagNames.owner: self.owner,
-            self.TagNames.blueprint: self.blueprint,
-            self.TagNames.sandbox_id: self.reservation_id,
-            self.TagNames.domain: self.domain,
-        }
