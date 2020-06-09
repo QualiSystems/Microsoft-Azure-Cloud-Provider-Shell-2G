eebd4370c497fa5f2deecde4c06212c82002484b
Anthony Poddubny <anton.p@qualisystems.com>
2020-06-09 23:15:25 +0300

    add AzureTaskWaiter class

--- src/package/cloudshell/cp/azure/flows/deploy_vm/commands/create_vm.py
+++ src/package/cloudshell/cp/azure/flows/deploy_vm/commands/create_vm.py
@@ -1,26 +1,27 @@
-import time
-from datetime import datetime, timedelta
-
 from package.cloudshell.cp.azure.utils.rollback import RollbackCommand
 
 
 class CreateVMCommand(RollbackCommand):
-    def __init__(self, rollback_manager, cancellation_manager, vm_actions, vm_name, virtual_machine, resource_group_name):
+    def __init__(self, rollback_manager, cancellation_manager, task_waiter_manager, vm_actions, vm_name,
+                 virtual_machine, resource_group_name):
         """
 
         :param rollback_manager:
         :param cancellation_manager:
+        :param task_waiter_manager:
         :param vm_name:
         :param virtual_machine:
         :param resource_group_name:
         """
         super().__init__(rollback_manager=rollback_manager, cancellation_manager=cancellation_manager)
+        self._task_waiter_manager = task_waiter_manager
         self._vm_actions = vm_actions
         self._vm_name = vm_name
         self._virtual_machine = virtual_machine
         self._resource_group_name = resource_group_name
 
     def _execute(self):
+        # todo: _expand_cloud_error_message ???
         # def _expand_cloud_error_message(self, exc, deployment_model):
         #     """
         #     :param CloudError exc:
@@ -41,30 +42,7 @@ class CreateVMCommand(RollbackCommand):
                                                                  virtual_machine=self._virtual_machine,
                                                                  resource_group_name=self._resource_group_name)
 
-        return self._wait_for_task(operation_poller)
+        return self._task_waiter_manager.wait_for_task(operation_poller)
 
     def rollback(self):
-        pass
-
-    # todo: move this function somewhere???
-    # todo: move values to constants
-    def _wait_for_task(self, operation_poller, wait_time=30, timeout=1800):
-        """Wait for Azure operation to be done
-
-        :param timeout:
-        :param operation_poller: msrestazure.azure_operation.AzureOperationPoller instance
-        :param wait_time: (int) seconds to wait before polling request
-        :return: Azure Operation Poller result
-        """
-        timeout_time = datetime.now() + timedelta(seconds=timeout)
-
-        while not operation_poller.done():
-            with self._cancellation_manager:
-                # self._logger.info(f"Waiting for operation to complete, current status is {operation_poller.status()}")
-                time.sleep(wait_time)
-
-            if datetime.now() > timeout_time:
-                raise Exception(f"Unable to perform operation within {timeout/60} minute(s)")
-
-        return operation_poller.result()
-
+        self._vm_actions.delete_vm(vm_name=self._vm_name, resource_group_name=self._resource_group_name)
--- src/package/cloudshell/cp/azure/flows/deploy_vm/commands/create_vm_extension.py
+++ src/package/cloudshell/cp/azure/flows/deploy_vm/commands/create_vm_extension.py
@@ -1,19 +1,16 @@
-import time
-from datetime import datetime, timedelta
-
 from azure.mgmt.compute.models import OperatingSystemTypes
 
 from package.cloudshell.cp.azure.utils.rollback import RollbackCommand
-from package.cloudshell.cp.azure.exceptions import AzureTaskTimeoutException
 
 
 class CreateVMExtensionCommand(RollbackCommand):
-    def __init__(self, rollback_manager, cancellation_manager, vm_extension_actions, script_file_path, script_config,
-                 timeout, image_os_type, region, resource_group_name, vm_name, tags):
+    def __init__(self, rollback_manager, cancellation_manager, task_waiter_manager, vm_extension_actions,
+                 script_file_path, script_config, timeout, image_os_type, region, resource_group_name, vm_name, tags):
         """
 
         :param rollback_manager:
         :param cancellation_manager:
+        :param task_waiter_manager:
         :param vm_extension_actions:
         :param script_file_path:
         :param script_config:
@@ -25,6 +22,7 @@ class CreateVMExtensionCommand(RollbackCommand):
         :param tags:
         """
         super().__init__(rollback_manager=rollback_manager, cancellation_manager=cancellation_manager)
+        self._task_waiter_manager = task_waiter_manager
         self._vm_extension_actions = vm_extension_actions
         self._script_file_path = script_file_path
         self._script_config = script_config
@@ -53,31 +51,7 @@ class CreateVMExtensionCommand(RollbackCommand):
                 script_config=self._script_config,
                 tags=self._tags)
 
-        # todo: check if self._timeout can be None here !!!!
-        return self._wait_for_task(operation_poller, timeout=self._timeout)
+        return self._task_waiter_manager.wait_for_task(operation_poller, timeout=self._timeout)
 
     def rollback(self):
         pass
-
-    # todo: move this function somewhere???
-    # todo: move values to constants
-    def _wait_for_task(self, operation_poller, wait_time=30, timeout=1800):
-        """Wait for Azure operation to be done
-
-        :param timeout:
-        :param operation_poller: msrestazure.azure_operation.AzureOperationPoller instance
-        :param wait_time: (int) seconds to wait before polling request
-        :return: Azure Operation Poller result
-        """
-        timeout_time = datetime.now() + timedelta(seconds=timeout)
-
-        while not operation_poller.done():
-            with self._cancellation_manager:
-                # self._logger.info(f"Waiting for operation to complete, current status is {operation_poller.status()}")
-                time.sleep(wait_time)
-
-            if datetime.now() > timeout_time:
-                raise AzureTaskTimeoutException(f"Unable to perform operation within {timeout/60} minute(s)")
-
-        return operation_poller.result()
-
