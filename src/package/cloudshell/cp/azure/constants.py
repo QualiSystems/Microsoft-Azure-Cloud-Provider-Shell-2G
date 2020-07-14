60fa89df2127f398425573faeb5afdb18c0eb29c
Anthony Poddubny <anton.p@qualisystems.com>
2020-07-14 15:28:47 +0300

    move code to the "cloudshell-cp-azure" package

--- src/package/cloudshell/cp/azure/constants.py
+++
@@ -1,4 +0,0 @@
-SHELL_NAME = "Microsoft Azure 2nd Gen"
-
-AZURE_VM_FROM_MARKETPLACE_DEPLOYMENT_PATH = f"{SHELL_NAME}.Azure VM From Marketplace 2nd Gen"
-AZURE_VM_FROM_CUSTOM_IMAGE_DEPLOYMENT_PATH = f"{SHELL_NAME}.Azure VM from Custom Image 2nd Gen"
