6db4e6d822347286dff6b90c207dfec65d88ea3a
alexquali <alexey.b@quali.com>
2022-12-08 14:29:11 +0200

    Fixed issues in refresh IP and getVmDetails

--- src/driver.py
+++ src/driver.py
@@ -53,8 +53,10 @@ import re
 import json
 import jsonpickle
 
+
 from cloudshell.cp.azure.actions.vm import VMActions
 from cloudshell.cp.azure.actions.vm_details import VMDetailsActions
+from cloudshell.cp.azure.actions.network import NetworkActions
 from cloudshell.cp.azure.utils.azure_name_parser import get_name_from_resource_id
 
 from cloudshell.cp.core.request_actions.models import (
@@ -415,7 +417,7 @@ class AzureDriver(ResourceDriverInterface):
                 resource=resource, cs_api=api
             )
 
-            refresh_ip_flow = AzureRefreshIPFlow(
+            refresh_ip_flow = StaticAzureRefreshIPFlow(
                 resource_config=resource_config,
                 azure_client=azure_client,
                 cs_api=api,
@@ -949,7 +951,8 @@ class AzureDriver(ResourceDriverInterface):
                 )
             )
             attr_mapping = {
-                "IP": "IPv4 Address",
+                "IP": "Private IP",
+                "Public IP": "Public IP",
                 "MAC Address": "MAC Address",
             }
 
@@ -970,9 +973,16 @@ class StaticGetVmDetailsFlow(AzureGetVMDetailsFlow):
     def _get_vm_details(self, deployed_app):
         """Get VM Details."""
         sandbox_resource_group_name = self._reservation_info.get_resource_group_name()
-        vm_resource_group_name = (
-            deployed_app.resource_group_name or sandbox_resource_group_name
-        )
+        if hasattr(deployed_app, "resource_group_name"):
+            vm_resource_group_name = (
+                deployed_app.resource_group_name or sandbox_resource_group_name
+            )
+            vm_name = deployed_app.name
+        else:
+            for k, v in deployed_app.attributes.items():
+                if k.endswith("VM Name"):
+                    vm_resource_group_name, vm_name = v.split("/")
+                    break
 
         vm_actions = VMActions(azure_client=self._azure_client, logger=self._logger)
         vm_details_actions = StaticVmDetailsActions(
@@ -981,7 +991,7 @@ class StaticGetVmDetailsFlow(AzureGetVMDetailsFlow):
 
         with self._cancellation_manager:
             vm = vm_actions.get_vm(
-                vm_name=deployed_app.name, resource_group_name=vm_resource_group_name
+                vm_name=vm_name, resource_group_name=vm_resource_group_name
             )
 
         if isinstance(deployed_app, AzureVMFromMarketplaceDeployedApp):
@@ -1005,6 +1015,31 @@ class StaticGetVmDetailsFlow(AzureGetVMDetailsFlow):
 
 
 class StaticVmDetailsActions(VMDetailsActions):
+    def _prepare_vm_details(
+        self,
+        virtual_machine,
+        resource_group_name: str,
+        prepare_vm_instance_data_function
+    ):
+        """Prepare VM details."""
+        try:
+            return VmDetailsData(
+                appName=virtual_machine.name,
+                vmInstanceData=prepare_vm_instance_data_function(
+                    virtual_machine=virtual_machine,
+                    resource_group_name=resource_group_name,
+                ),
+                vmNetworkData=self._prepare_vm_network_data(
+                    virtual_machine=virtual_machine,
+                    resource_group_name=resource_group_name,
+                ),
+            )
+        except Exception as e:
+            self._logger.exception(
+                f"Error getting VM details for {virtual_machine.name}"
+            )
+            return VmDetailsData(appName=virtual_machine.name, errorMessage=str(e))
+
     def _prepare_vm_network_data(self, virtual_machine, resource_group_name):
         """Prepare VM Network data.
 
@@ -1060,3 +1095,74 @@ class StaticVmDetailsActions(VMDetailsActions):
             vm_network_interfaces.append(vm_network_interface)
 
         return vm_network_interfaces
+
+
+class StaticAzureRefreshIPFlow(AzureRefreshIPFlow):
+    def refresh_ip(self, deployed_app):
+        """Refresh Public and Private IPs on the CloudShell resource."""
+        sandbox_resource_group_name = self._reservation_info.get_resource_group_name()
+        if hasattr(deployed_app, "resource_group_name"):
+            vm_resource_group_name = (
+                deployed_app.resource_group_name or sandbox_resource_group_name
+            )
+            vm_name = deployed_app.name
+        else:
+            for k, v in deployed_app.attributes.items():
+                if k.endswith("VM Name"):
+                    vm_resource_group_name, vm_name = v.split("/")
+                    break
+
+        vm_actions = VMActions(azure_client=self._azure_client, logger=self._logger)
+        network_actions = NetworkActions(
+            azure_client=self._azure_client, logger=self._logger
+        )
+
+        vm = vm_actions.get_active_vm(
+            vm_name=vm_name, resource_group_name=vm_resource_group_name
+        )
+
+        primary_interface_ref = self._get_primary_vm_interface(vm)
+        interface_name = get_name_from_resource_id(primary_interface_ref.id)
+
+        vm_network = network_actions.get_vm_network(
+            interface_name=interface_name, resource_group_name=vm_resource_group_name
+        )
+
+        vm_ip_configuration = vm_network.ip_configurations[0]
+        private_ip_on_azure = vm_ip_configuration.private_ip_address
+        public_ip_reference = vm_ip_configuration.public_ip_address
+
+        if public_ip_reference is None:
+            self._logger.info(
+                f"There is no Public IP attached to the VM {deployed_app.name}"
+            )
+            public_ip_on_azure = ""
+        else:
+            self._logger.info(f"Retrieving Public IP for the VM {deployed_app.name}")
+            pub_ip_addr = self._azure_client.get_public_ip(
+                public_ip_name=vm_ip_configuration.public_ip_address.name,
+                resource_group_name=vm_resource_group_name,
+            )
+
+            public_ip_on_azure = pub_ip_addr.ip_address
+
+        self._logger.info(f"Public IP on Azure: {public_ip_on_azure}")
+        self._logger.info(f"Public IP on CloudShell: {deployed_app.public_ip}")
+
+        if public_ip_on_azure != deployed_app.public_ip:
+            self._logger.info(
+                f"Updating Public IP on the VM {deployed_app.name} "
+                f"to {public_ip_on_azure}"
+            )
+            deployed_app.update_public_ip(public_ip_on_azure)
+
+        self._logger.info(f"Private IP on Azure: {private_ip_on_azure}")
+        self._logger.info(f"Private IP on CloudShell: {deployed_app.private_ip}")
+
+        if private_ip_on_azure != deployed_app.private_ip:
+            self._logger.info(
+                f"Updating Private IP on the resource to {private_ip_on_azure}"
+            )
+            self._cs_api.UpdateResourceAddress(
+                resourceFullPath=deployed_app.name, resourceAddress=private_ip_on_azure
+            )
